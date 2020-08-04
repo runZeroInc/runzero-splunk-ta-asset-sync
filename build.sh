22a64c0a4ee5aaf007c4a9060f41491b2a9bb6fd
HD Moore <x@hdm.io>
2020-08-04 12:42:41 -0500

    bump to 1.0.4

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-tar cf TA_rumble_asset_sync.tar TA_rumble_asset_sync && gzip -9 TA_rumble_asset_sync.tar && mv TA_rumble_asset_sync.tar.gz TA_rumble_asset_sync-1.0.3.spl
+tar cf TA_rumble_asset_sync.tar TA_rumble_asset_sync && gzip -9 TA_rumble_asset_sync.tar && mv TA_rumble_asset_sync.tar.gz TA_rumble_asset_sync-1.0.4.spl
