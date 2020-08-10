e6cfdc7b46855d25865a302faf29c2c006fa7f10
HD Moore <x@hdm.io>
2020-08-10 08:46:03 -0500

    build 1.0.5

--- TA_rumble_asset_sync/bin/ta_rumble_asset_sync/aob_py3/mako/template.py
+++ TA_rumble_asset_sync/bin/ta_rumble_asset_sync/aob_py3/mako/template.py
@@ -503,7 +503,7 @@ class Template(object):
     def list_defs(self):
         """return a list of defs in the template.
 
-        .. versionadded:: 1.0.4
+        .. versionadded:: 1.0.0
 
         """
         return [i[7:] for i in dir(self.module) if i[:7] == "render_"]
--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 rm -f TA_rumble_asset_sync.tar TA_rumble_asset_sync.tar.gz *.spl
-tar cf TA_rumble_asset_sync.tar TA_rumble_asset_sync && gzip -9 TA_rumble_asset_sync.tar && mv TA_rumble_asset_sync.tar.gz TA_rumble_asset_sync-1.0.4.spl
+tar cf TA_rumble_asset_sync.tar TA_rumble_asset_sync && gzip -9 TA_rumble_asset_sync.tar && mv TA_rumble_asset_sync.tar.gz TA_rumble_asset_sync-1.0.5.spl
