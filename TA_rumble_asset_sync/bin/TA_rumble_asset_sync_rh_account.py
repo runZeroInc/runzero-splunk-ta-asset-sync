eb185aa98675a978c8f7bd5bad193eb329aed6fe
HD Moore <x@hdm.io>
2020-08-17 11:17:22 -0500

    move api_key to a global account setting

---
+++ TA_rumble_asset_sync/bin/TA_rumble_asset_sync_rh_account.py
@@ -0,0 +1,42 @@
+
+import ta_rumble_asset_sync_declare
+
+from splunktaucclib.rest_handler.endpoint import (
+    field,
+    validator,
+    RestModel,
+    SingleModel,
+)
+from splunktaucclib.rest_handler import admin_external, util
+from splunk_aoblib.rest_migration import ConfigMigrationHandler
+
+util.remove_http_proxy_env_vars()
+
+
+fields = [
+    field.RestField(
+        'api_key',
+        required=True,
+        encrypted=True,
+        default=None,
+        validator=validator.String(
+            min_len=16, 
+            max_len=256,
+        )
+    )
+]
+model = RestModel(fields, name=None)
+
+
+endpoint = SingleModel(
+    'ta_rumble_asset_sync_account',
+    model,
+)
+
+
+if __name__ == '__main__':
+    admin_external.handle(
+        endpoint,
+        handler=ConfigMigrationHandler,
+    )
+
--- TA_rumble_asset_sync/bin/TA_rumble_asset_sync_rh_assets.py
+++ TA_rumble_asset_sync/bin/TA_rumble_asset_sync_rh_assets.py
@@ -20,64 +20,60 @@ fields = [
         encrypted=False,
         default=None,
         validator=validator.Pattern(
-            regex=r"""^\-[1-9]\d*$|^\d*$""", 
+            regex=r"""^\-[1-9]\d*$|^\d*$""",
         )
-    ), 
+    ),
     field.RestField(
         'index',
         required=True,
         encrypted=False,
         default='default',
         validator=validator.String(
-            min_len=1, 
-            max_len=80, 
+            min_len=1,
+            max_len=80,
         )
-    ), 
+    ),
     field.RestField(
-        'api_key',
+        'global_account',
         required=True,
-        encrypted=True,
+        encrypted=False,
         default=None,
-        validator=validator.String(
-            min_len=0, 
-            max_len=8192, 
-        )
-    ), 
+        validator=None
+    ),
     field.RestField(
         'sync_type',
         required=True,
         encrypted=False,
         default='created',
         validator=None
-    ), 
+    ),
     field.RestField(
         'search_filter',
         required=False,
         encrypted=False,
         default=None,
         validator=validator.String(
-            min_len=0, 
-            max_len=8192, 
+            min_len=0,
+            max_len=8192,
         )
-    ), 
+    ),
     field.RestField(
         'since',
         required=True,
         encrypted=False,
         default=None,
         validator=validator.String(
-            min_len=0, 
-            max_len=8192, 
+            min_len=0,
+            max_len=8192,
         )
-    ), 
+    ),
     field.RestField(
         'builtin_system_checkpoint_storage_type',
         required=False,
         encrypted=False,
         default='auto',
         validator=None
-    ), 
-
+    ),
     field.RestField(
         'disabled',
         required=False,
