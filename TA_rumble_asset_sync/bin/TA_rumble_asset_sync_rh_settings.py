400a1d8a034a147dd0231a23e67e5d853a235fd0
HD Moore <x@hdm.io>
2021-04-04 13:28:34 -0500

    add proxy support to the rumble splunk addon

--- TA_rumble_asset_sync/bin/TA_rumble_asset_sync_rh_settings.py
+++ TA_rumble_asset_sync/bin/TA_rumble_asset_sync_rh_settings.py
@@ -7,10 +7,78 @@ from splunktaucclib.rest_handler.endpoint import (
     RestModel,
     MultipleModel,
 )
+
 from splunktaucclib.rest_handler import admin_external, util
+from splunktaucclib.rest_handler.admin_external import AdminExternalHandler
 from splunk_aoblib.rest_migration import ConfigMigrationHandler
+from splunktaucclib.rest_handler.endpoint.validator import Validator
 
 util.remove_http_proxy_env_vars()
+
+fields_proxy = [
+    field.RestField(
+        'proxy_enabled',
+        required=False,
+        encrypted=False,
+        default=None,
+        validator=None
+    ), 
+    field.RestField(
+        'proxy_type',
+        required=True,
+        encrypted=False,
+        default='http',
+        validator=None
+    ), 
+    field.RestField(
+        'proxy_rdns',
+        required=False,
+        encrypted=False,
+        default=None,
+        validator=None
+    ), 
+    field.RestField(
+        'proxy_url',
+        required=False,
+        encrypted=False,
+        default=None,
+        validator=validator.String(
+            max_len=4096, 
+            min_len=0, 
+        )
+    ), 
+    field.RestField(
+        'proxy_port',
+        required=False,
+        encrypted=False,
+        default=None,
+        validator=validator.Number(
+            max_val=65535, 
+            min_val=1, 
+        )
+    ), 
+    field.RestField(
+        'proxy_username',
+        required=False,
+        encrypted=False,
+        default=None,
+        validator=validator.String(
+            min_len=0, 
+            max_len=256, 
+        )
+    ), 
+    field.RestField(
+        'proxy_password',
+        required=False,
+        encrypted=True,
+        default=None,
+        validator=validator.String(
+            min_len=0, 
+            max_len=256, 
+        )
+    )
+]
+model_proxy = RestModel(fields_proxy, name='proxy')
 
 
 fields_logging = [
@@ -24,14 +92,13 @@ fields_logging = [
 ]
 model_logging = RestModel(fields_logging, name='logging')
 
-
 endpoint = MultipleModel(
     'ta_rumble_asset_sync_settings',
     models=[
+        model_proxy, 
         model_logging
     ],
 )
-
 
 if __name__ == '__main__':
     admin_external.handle(
