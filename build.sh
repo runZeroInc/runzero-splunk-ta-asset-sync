5f388a0909bf51988bbae301a61de5a3f2a2585c
Andrew Click-Horn <andrew@runzero.com>
2022-08-30 13:39:31 -0700

    Rename folders referencing `rumble`

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 rm -f TA_runzero_asset_sync.tar TA_runzero_asset_sync.tar.gz *.spl
-tar cf TA_runzero_asset_sync.tar TA_runzero_asset_sync && gzip -9 TA_runzero_asset_sync.tar && mv TA_runzero_asset_sync.tar.gz TA_runzero_asset_sync-3.0.2.spl
+tar cf TA_runzero_asset_sync.tar TA_runzero_asset_sync && gzip -9 TA_runzero_asset_sync.tar && mv TA_runzero_asset_sync.tar.gz TA_runzero_asset_sync-3.0.3.spl
