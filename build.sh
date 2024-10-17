c7f61bbef157ba819d643c4aa573fbc050b4ccb6
Brandon Turner <brandon.turner@runzero.com>
2024-10-16 23:39:06 -0500

    Bump version to 3.1.1

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 rm -f TA_runzero_asset_sync.tar TA_runzero_asset_sync.tar.gz *.spl
-COPYFILE_DISABLE=1 tar --exclude='.*' --exclude='.*/' -cf TA_runzero_asset_sync.tar TA_runzero_asset_sync && gzip -9 TA_runzero_asset_sync.tar && mv TA_runzero_asset_sync.tar.gz TA_runzero_asset_sync-3.1.0.spl
+COPYFILE_DISABLE=1 tar --exclude='.*' --exclude='.*/' -cf TA_runzero_asset_sync.tar TA_runzero_asset_sync && gzip -9 TA_runzero_asset_sync.tar && mv TA_runzero_asset_sync.tar.gz TA_runzero_asset_sync-3.1.1.spl
